254e239e0a57ee34dc7abc86b0fd616d92b17fb5
Matias Galarza <lobinuxsoft@gmail.com>
2022-04-19 11:22:29 -0300

    Modify quit behaviour

--- Assets/Script/QuitGame.cs
+++ Assets/Script/QuitGame.cs
@@ -1,9 +1,22 @@
+using System;
 using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
+    private void Awake()
+    {
+
+#if UNITY_WEBGL
+         gameObject.SetActive(false);
+#endif
+    }
+
     public void ExitGame()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 }
